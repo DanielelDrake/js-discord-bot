b89d5c655b81627ad412b0c0b3b3e93d1b724a46
Daniel Habenicht <habenicht.daniel@t-online.de>
2022-09-28 18:05:59 +0200

    first functions

--- bot.js
+++ bot.js
@@ -7,13 +7,29 @@ const client = new Discord.Client({
       GatewayIntentBits.Guilds,
       GatewayIntentBits.GuildMessages,
     ]
-  })
+})
+
+const prefix = "!";
 
 
+client.on("ready", () => {
+console.log(`Logged in as ${client.user.tag}!`)
+})
 
-  client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-  })
+client.on("messageCreate", function(message) {
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLocaleLowerCase();
+
+    if(command == "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply('pong!');
+        console.log('Message had the latency od ${timeTaken}ms.');
+    }
+})
 
 
 client.login(config.BOT_TOKEN);
