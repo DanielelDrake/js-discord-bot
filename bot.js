7b39ac427a47c4dfb35c016f387dd935e289bc67
Daniel Habenicht <habenicht.daniel@t-online.de>
2022-09-29 20:36:30 +0200

    updated functions

--- bot.js
+++ bot.js
@@ -19,15 +19,28 @@ client.on("ready", () => {
 console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("messageCreate", (msg) => {
+client.on("guildMemberRemove", () => {
+    console.log('Member removed!')
+})
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.reply("Welcome!")
+})
+
+client.on("messageCreate", (msg, member) => {
     if(msg.author.bot) {
         return
-    } else {
-        
-        if(msg.content === 'test'){
+    } 
+    else {
+        if(msg.content === 'role pls') {
+            console.log('roles updated')
+           // msg.author.roles.add(author.guild.roles.cache.find(i => i.name === 'Member'))
+        }
+        else if(msg.content === 'test'){
             console.log('testing');
             msg.reply("testing")
-        }else {
+        }
+        else{
             console.log("random msg")
             console.log(msg)
 
