6caa4679854ca973bd977b5a279949c26fd98447
Daniel Habenicht <habenicht.daniel@t-online.de>
2022-10-04 13:18:12 +0200

    Update commands_info.js

--- commands_info.js
+++ commands_info.js
@@ -21,6 +21,6 @@ module.exports = {
             .setTimestamp()
             .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
-        msg.reply({ embeds: [exampleEmbed] });
+        console.log("command List")
     }
 }
