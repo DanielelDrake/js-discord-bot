864db2fc5c5fd4b912c472b0485128f8ac0720ba
Daniel Habenicht <habenicht.daniel@t-online.de>
2022-10-04 13:09:08 +0200

    Update commands_info.js

--- commands_info.js
+++ commands_info.js
@@ -1,5 +1,9 @@
 module.exports = {
     commandList: function () {
-        
+        let embed = new MessageEmbed()
+            .setTitle("Command List")
+            .setDescription("!help, !roll, !kick, !ban")
+            .setColor("RANDOM")
+        message.channel.send(embed)
     }
 }
