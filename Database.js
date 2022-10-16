b0f555aca473b4232884b1f71900fbd10739685b
Daniel Habenicht <habenicht.daniel@t-online.de>
2022-10-17 00:54:35 +0200

    Update Database.js

--- Database.js
+++ Database.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2/promise');
 
 mysql.createConnection({
+    host:"localhost",
+    debug: false,
     user: "Daniel",
     password: "Wirklichreal1?"
 }).then(() => console.log('goof')).catch(err => console.log(err));
