25abb22afe900a8f019168101d75b2ba11870f52
Daniel Habenicht <habenicht.daniel@t-online.de>
2022-10-16 21:01:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,10 +31,8 @@ async function asyncFunction() {
   let conn;
   try {
 	conn = await pool.getConnection();
-	const rows = await conn.query("SELECT 1 as val");
+	const rows = await conn.query("SELECT * from user");
 	console.log(rows); //[ {val: 1}, meta: ... ]
-	const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-	console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
 
   } catch (err) {
 	throw err;
