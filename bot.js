741c98ee49478837f249645c8a3f64f4392596e2
Daniel Habenicht <habenicht.daniel@t-online.de>
2022-10-17 13:53:22 +0200

    Shlash Command Testing

--- bot.js
+++ bot.js
@@ -1,10 +1,9 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
 const Discord = require("discord.js");
 const config = require("./config.json");
-const { MessageEmbed, makeURLSearchParams, EmbedBuilder } = require("discord.js")
-
-
-const { Client, GatewayIntentBits, Collection, messageLink, channelLink } = require('discord.js');
-const { connect } = require('mongoose');
+const { MessageEmbed, makeURLSearchParams, EmbedBuilder, Client, GatewayIntentBits, Collection, messageLink, channelLink } = require("discord.js")
 const client = new Discord.Client({
     intents: [
         GatewayIntentBits.DirectMessages,
@@ -15,19 +14,57 @@ const client = new Discord.Client({
     ]
 });
 
+client.commands = new Collection();
 
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+const { connect } = require('mongoose');
 const mongoose = require('mongoose');
 const User = require('./schemas/UserSchema')
+
+//command handling
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+
+    console.log(interaction)
+})
 
 
 //async functions
 client.on("ready", async () => {
     //DB-Connect
     await mongoose.connect(config.MONGO_URL, {
-            keepAlive: true
-        })
+        keepAlive: true
+    })
     console.log('Connected to DB')
     console.log(`Logged in as ${client.user.tag}!`)
+    console.log('-----------------------------------------')
 })
 
 client.on("guildMemberAdd", async (member) => {
@@ -42,6 +79,7 @@ client.on("guildMemberAdd", async (member) => {
         joined_at: Date.now
     })
     console.log('neuen Datensatz für User: ' + member.username + ' angelegt')
+    console.log('--------------------------------------------')
 })
 
 //prefix for commands
@@ -60,10 +98,10 @@ client.on("messageCreate", async (msg) => {
     if (command === 'test') {
         console.log("test requested")
     }
-        //Server-Status
-        //eigene User Info
-        //help
-        //
+    //Server-Status
+    //eigene User Info
+    //help
+    //
 
     //admin commands
     if (command === 'ban') {
@@ -80,13 +118,13 @@ client.on("messageCreate", async (msg) => {
                 let victim = msg.mentions.members.first();
                 msg.reply("User " + victim.user.username + " got banned by " + msg.author.username)
                 victim.ban();
-
             }
         }
         else {
             //respond with error message
             msg.reply("Da hast du wohl nicht die nötigen Rechte... Wende dich an einen Admin!")
         }
+        console.log('---------------------------------------------------------')
     }
     if (command === 'kick') {
         //check for Admin-Permissions
@@ -108,18 +146,21 @@ client.on("messageCreate", async (msg) => {
             //respond with error message
             msg.reply("Da hast du wohl nicht die nötigen Rechte... Wende dich an einen Admin!")
         }
+        console.log('---------------------------------------------------------')
     }
     if (command === 'db') {
         if (msg.member.roles.cache.has('1025086853742866532')) {
-            console.log(msg.author.username + " is searching for User...")
+            console.log(msg.author.username + " is searching for User " + args[0])
             const res = await User.find({ username: args[0] });
             console.log('Username: ' + res[0].username)
             console.log('Users DiscordId: ' + res[0].discordId)
             console.log('Admin status: ' + res[0].admin)
             console.log('Member since: ' + res[0].joined_at)
+            console.log('-------------------------------------------')
         } else {
             console.log(msg.author.username + " hat versucht auf die DB zuzugreifen!")
             msg.reply("Du hast nicht die nötigen Berechtigungen auf diesem Server!")
+            console.log('-------------------------------------------')
         }
 
     }
