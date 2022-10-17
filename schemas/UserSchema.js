0d0114f57da69e7da72a00330b66249726cadde7
Daniel Habenicht <habenicht.daniel@t-online.de>
2022-10-17 08:58:18 +0200

    Update UserSchema.js

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -7,6 +7,7 @@ const UserSchema = new mongoose.Schema({
         required: true,
     },
     admin: mongoose.SchemaTypes.Boolean,
+    joined_at: mongoose.SchemaTypes.Date,
 })
 
 module.exports = mongoose.model('User', UserSchema);
