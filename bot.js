22deec16bd36e8dc387b6dc8c7bee08fe18d007d
Daniel Habenicht <habenicht.daniel@t-online.de>
2022-10-20 17:26:46 +0200

    working slash commands ++ cleanup

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
 const fs = require('node:fs');
 const path = require('node:path');
-
+const { connect } = require('mongoose');
+const mongoose = require('mongoose');
+const User = require('./schemas/UserSchema')
 const Discord = require("discord.js");
 const config = require("./config.json");
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 const { MessageEmbed, makeURLSearchParams, EmbedBuilder, Client, GatewayIntentBits, Collection, messageLink, channelLink } = require("discord.js")
 const client = new Discord.Client({
     intents: [
@@ -16,8 +20,7 @@ const client = new Discord.Client({
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
@@ -30,9 +33,7 @@ for (const file of commandFiles) {
 	}
 }
 
-const { connect } = require('mongoose');
-const mongoose = require('mongoose');
-const User = require('./schemas/UserSchema')
+
 
 //command handling
 client.on('interactionCreate', async interaction => {
@@ -47,12 +48,12 @@ client.on('interactionCreate', async interaction => {
 
 	try {
 		await command.execute(interaction);
+        console.log(`${interaction.commandName} command was run by ${interaction.user.username}`)
 	} catch (error) {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
-
-    console.log(interaction)
+    
 })
 
 
@@ -80,93 +81,6 @@ client.on("guildMemberAdd", async (member) => {
     })
     console.log('neuen Datensatz für User: ' + member.username + ' angelegt')
     console.log('--------------------------------------------')
-})
-
-//prefix for commands
-const prefix = "!";
-
-//user commands -> prefix+commmand+arg
-client.on("messageCreate", async (msg) => {
-
-    if (msg.author.bot) return;
-    if (msg.content.indexOf(prefix) !== 0) return;
-
-    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    //comands:
-    if (command === 'test') {
-        console.log("test requested")
-    }
-    //Server-Status
-    //eigene User Info
-    //help
-    //
-
-    //admin commands
-    if (command === 'ban') {
-        //check for Admin-Permissions
-        if (msg.member.roles.cache.has('1025086853742866532')) {
-            //log the ban-attempt
-            console.log(msg.author.username + " wants to ban somebody")
-            //check for args
-            if (!args.length) {
-                return msg.reply(`You didn't provide any arguments, ${msg.author}!`);
-            }
-            else {
-                //perform ban
-                let victim = msg.mentions.members.first();
-                msg.reply("User " + victim.user.username + " got banned by " + msg.author.username)
-                victim.ban();
-            }
-        }
-        else {
-            //respond with error message
-            msg.reply("Da hast du wohl nicht die nötigen Rechte... Wende dich an einen Admin!")
-        }
-        console.log('---------------------------------------------------------')
-    }
-    if (command === 'kick') {
-        //check for Admin-Permissions
-        if (msg.member.roles.cache.has('1025086853742866532')) {
-            //log the kick-attempt
-            console.log(msg.author.username + " wants to kick somebody")
-            //check for args
-            if (!args.length) {
-                return msg.reply("You didn't provide any Arguments!")
-            }
-            else {
-                //perform the kick
-                let victim = msg.mentions.members.first();
-                msg.reply("User " + victim.user.username + " got kicked by " + msg.author.username)
-                victim.kick();
-            }
-        }
-        else {
-            //respond with error message
-            msg.reply("Da hast du wohl nicht die nötigen Rechte... Wende dich an einen Admin!")
-        }
-        console.log('---------------------------------------------------------')
-    }
-    if (command === 'db') {
-        if (msg.member.roles.cache.has('1025086853742866532')) {
-            console.log(msg.author.username + " is searching for User " + args[0])
-            const res = await User.find({ username: args[0] });
-            console.log('Username: ' + res[0].username)
-            console.log('Users DiscordId: ' + res[0].discordId)
-            console.log('Admin status: ' + res[0].admin)
-            console.log('Member since: ' + res[0].joined_at)
-            console.log('-------------------------------------------')
-        } else {
-            console.log(msg.author.username + " hat versucht auf die DB zuzugreifen!")
-            msg.reply("Du hast nicht die nötigen Berechtigungen auf diesem Server!")
-            console.log('-------------------------------------------')
-        }
-
-    }
-    if (command === 'setmod') {
-
-    }
 })
 
 //passive Chat-Moderation
