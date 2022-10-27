3063ebcff1a88a4ac368f80f8503155397c349dc
Daniel Habenicht <habenicht.daniel@t-online.de>
2022-10-27 16:29:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
-import { readdirSync } from 'node:fs';
-import { join } from 'node:path';
-import { connect } from 'mongoose';
-import { connect as _connect } from 'mongoose';
-import { create } from './schemas/UserSchema';
-import { Client as _Client } from "discord.js";
-import { MONGO_URL, BOT_TOKEN } from "./config.json";
-const commandsPath = join(__dirname, 'commands');
+const fs = require('node:fs');
+const path = require('node:path');
+const { connect } = require('mongoose');
+const mongoose = require('mongoose');
+const User = require('./schemas/UserSchema')
+const Discord = require("discord.js");
+const config = require("./config.json");
+const commandsPath = path.join(__dirname, 'commands');
 
-const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-import { MessageEmbed, makeURLSearchParams, EmbedBuilder, Client, GatewayIntentBits, Collection, messageLink, channelLink } from "discord.js";
-const client = new _Client({
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const { MessageEmbed, makeURLSearchParams, EmbedBuilder, Client, GatewayIntentBits, Collection, messageLink, channelLink } = require("discord.js")
+const client = new Discord.Client({
     intents: [
         GatewayIntentBits.DirectMessages,
         GatewayIntentBits.Guilds,
@@ -22,7 +22,7 @@ const client = new _Client({
 client.commands = new Collection();
 
 for (const file of commandFiles) {
-	const filePath = join(commandsPath, file);
+	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	// Set a new item in the Collection with the key as the command name and the value as the exported module
 	if ('data' in command && 'execute' in command) {
@@ -56,7 +56,7 @@ client.on('interactionCreate', async interaction => {
 //async functions
 client.on("ready", async () => {
     //DB-Connect
-    await _connect(MONGO_URL, {
+    await mongoose.connect(config.MONGO_URL, {
         keepAlive: true
     })
     console.log('Connected to DB')
@@ -69,7 +69,7 @@ client.on("guildMemberAdd", async (member) => {
 
     //neuen User in der Datenbank anlegen
     console.log('neuen Datensatz für ' + member.username + ' anlegen ...');
-    const newUser = await create({
+    const newUser = await User.create({
         username: member.username,
         discordId: member.id,
         admin: false,
@@ -90,5 +90,5 @@ client.on("messageCreate", (msg) => {
 })
 
 //login
-client.login(BOT_TOKEN);
+client.login(config.BOT_TOKEN);
 
