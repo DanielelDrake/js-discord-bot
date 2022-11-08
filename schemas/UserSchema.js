c8894650a0615467101d5587e65530d14378280e
Daniel Habenicht <danielhabenicht@icloud.com>
2022-11-08 18:55:35 +0100

    Update UserSchema.js

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: mongoose.mongoose.SchemaTypes.String,
+    username: {
+        type: mongoose.mongoose.SchemaTypes.String,
+        required: true,
+    },
     discordId : {
         type: mongoose.SchemaTypes.String,
         required: true,
     },
     admin: mongoose.SchemaTypes.Boolean,
     mod: mongoose.SchemaTypes.Boolean,
-    joined_at: mongoose.SchemaTypes.Date,
+    joined_at: {
+        type: mongoose.SchemaTypes.Date,
+        required: true,
+    },
 })
 
 module.exports = mongoose.model('User', UserSchema);
